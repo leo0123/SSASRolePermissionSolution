640df416a70adae2f54993102fee1e5b0c30457a
Leo <leo.c.chen@deltaww.com>
2016-06-20 11:28:15 -0700

    2nd

---
+++ SqlToMdxWinform/Form1.cs
@@ -0,0 +1,32 @@
+﻿using SqlToMdxLib.DAL;
+using SqlToMdxLib.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace SqlToMdxWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //UsersLineContext db = new UsersLineContext();
+            //var q = db.UsersLinePs.Count();//
+            SqlToMdxLib.SqlToMdx a = new SqlToMdxLib.SqlToMdx();
+            a.replaceAll();
+        }
+    }
+}
---
+++ UsersLineManageService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UsersLineManageService.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
